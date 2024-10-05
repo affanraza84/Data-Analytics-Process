efe4824ef1903acb948331207421ae3bedbc37fa
Mohammad Affan Raza <160397447+affanraza84@users.noreply.github.com>
2024-10-05 10:37:12 +0530

    Add files via upload

---
+++ work_04/script.js
@@ -0,0 +1,14 @@
+gsap.from("#heading h1",{
+    y:-108,
+    duration:0.8,
+    delay:1,
+    stagge:2,
+    color:"#fff"
+})
+
+gsap.from("#circle",{
+    opacity:0,
+    size:0,
+    delay:1,
+    duration:1,
+})
